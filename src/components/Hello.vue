8a3ace502e45232ec3e7c6fadaeddcd93822c415
seekerliu <seekerliu@vip.qq.com>
2017-09-21 18:51:03 +0800

    :tada: first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>A PHP Artisan.</p>
+    <h2>Lessons</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/seekerliu/laravel-tips" target="_blank">laravel-tips</a>
+      </li>
+    </ul>
+    <h2>Packages</h2>
+    <ul>
+
+      <li>
+        <a href="https://github.com/seekerliu/laravel-another-json" target="_blank">laravel-another-json</a>
+      </li>
+      <li>
+        <a href="https://github.com/seekerliu/laravel-readme-viewer" target="_blank">laravel-readme-viewer</a>
+      </li>
+      <li>
+        <a href="https://github.com/seekerliu/laravel-ddns" target="_blank">laravel-ddns</a>
+      </li>
+      <li>
+        <a href="https://github.com/seekerliu/laravel-view-composer-generator" target="_blank">laravel-view-composer-generator</a>
+      </li>
+    </ul>
+    <h2>Projects</h2>
+    <ul>
+      <li><a href="https://huosuxue.com/" target="_blank">火速学</a></li>
+      <li><a href="https://youdua.com/" target="_blank">有度啊</a></li>
+    </ul>
+    <h2>Contacts</h2>
+    <p>seekerliu@vip.qq.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'SeekerLiu'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #0366d6;
+}
+</style>
